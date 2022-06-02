9d2340e126ada2279067dc00b91a6c29e42840f1
Luke Wong <luke@hiredly.com>
2022-06-02 13:22:31 +0800

    Add margin bottom to each card

--- src/components/RestaurantCards.js
+++ src/components/RestaurantCards.js
@@ -8,15 +8,23 @@ import {
 } from "@mui/material";
 
 function RestaurantCards(props) {
+
   return (
     <>
       {props.restaurants.map((restaurant) => (
-        <Card key={restaurant.id}>
+        <Card key={restaurant.id} style={{ marginBottom: "1rem" }}>
           <CardContent>
-            <Typography variant="subtitle1">Name:{" "}{restaurant.name}</Typography>
-            <Typography variant="subtitle1">Cuisine:{" "}{restaurant.cuisine}</Typography>
-            <Typography variant="subtitle1">Location:{" "}{restaurant.location}</Typography>
-            <Typography variant="subtitle1">Comments:{" "}{restaurant.comment}</Typography>
+            <Typography variant="subtitle1">ID: {restaurant.id}</Typography>
+            <Typography variant="subtitle1">Name: {restaurant.name}</Typography>
+            <Typography variant="subtitle1">
+              Cuisine: {restaurant.cuisine}
+            </Typography>
+            <Typography variant="subtitle1">
+              Location: {restaurant.location}
+            </Typography>
+            <Typography variant="subtitle1">
+              Comments: {restaurant.comment}
+            </Typography>
             <CardActions>
               <Button>remove restaurant</Button>
             </CardActions>
